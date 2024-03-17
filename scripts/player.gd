20f0d76cee639875146055768c236e69a4fe9c63
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-18 09:55:43 +1300

    working

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,22 +2,34 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -800.0
+const JUMP_VELOCITY = -600.0
 
 var start_position = Vector2(600,250)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		if abs(velocity.x) > 10:
+			anim.play("run")
+		else:
+			anim.play("idle")
+
+	if velocity.x < 0:
+		anim.flip_h = true
+	else:
+		anim.flip_h = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,6 +41,10 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+	
 	#respawn
 	if position.y > 900:
 		position = start_position
+
+func respawn():
+	position = start_position
